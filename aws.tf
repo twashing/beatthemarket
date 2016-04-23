8c1aad400842b623906ea87668e14c845147a59e
Timothy Washington <twashing@interruptsoftware.com>
2016-04-23 09:55:59 -0700

    -- fixed a bug in a load balancer's ontainer_name -- troubleshooting a role error in service

--- aws.tf
+++ aws.tf
@@ -38,8 +38,27 @@ resource "aws_elb" "beathemarket-elb" {
   }
 }
 
-resource "aws_iam_role" "beathemarket-role" {
-    name = "beathemarket-role"
+resource "aws_iam_role_policy" "beatthemarket_policy" {
+    name = "beatthemarket_policy"
+    role = "${aws_iam_role.beatthemarket_role.id}"
+    policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "beatthemarket_role" {
+    name = "beatthemarket_role"
     assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -67,10 +86,10 @@ resource "aws_ecs_service" "beatthemarket_service" {
   cluster         = "${aws_ecs_cluster.default.id}"
   task_definition = "${aws_ecs_task_definition.beatthemarket-task.arn}"
   desired_count   = 1
-  iam_role = "${aws_iam_role.beathemarket-role.arn}"
+  iam_role = "${aws_iam_role.beatthemarket_role.arn}"
   load_balancer {
     elb_name = "${aws_elb.beathemarket-elb.name}"
-    container_name = "beatthemarket_service"
+    container_name = "beatthemarket"
     container_port = 8080
   }
 }
